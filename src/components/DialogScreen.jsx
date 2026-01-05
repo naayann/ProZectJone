c0bac49f540bca32384e469ba3d0069489e35482
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-01-05 17:19:48 +0530

    Design dialog screen

--- src/components/DialogScreen.jsx
+++ src/components/DialogScreen.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import logo from '../assets/logo.jpg'
+import logo from '../assets/logo-light.png'
+import title from '../assets/Title.png'
 
 const DialogScreen = () => {
   return (
     <div>
-      <div className='flex justify-end mr-5 mt-3'>
-        <p className=''>Skip</p>
+      <div className='flex justify-end fixed top-0 right-0 p-5'>
+        <p className='font-medium'>Skip</p>
       </div>
-      <div className='flex justify-center items-center flex-col'>
-        <p>ProZectJone</p>
-        <img src={logo} className='invert w-25' />
+      <div className='flex justify-center items-center flex-col mt-25'>
+        <img src={title} className='w-80'/>
+        <img src={logo} className='invert w-50' />
       </div>
-      <div>
-        <h1>ProZectJone</h1>
-        <p>prozectjone is a site where you can share your crazy project ideas, and see it come to live.
-          -the name prozectjone is derived from the word "project zone"
+      <div className='fixed bottom-0 mb-10 ml-4'>
+        <h1 className='font-semibold text-blue-600 text-lg'>ProZectJone</h1>
+        <p className='text-xs font-medium '>prozectjone is a site where you can share your crazy project ideas, and see it come to live.
+          <br />-the name prozectjone is derived from "project zone".
         </p>
       </div>
     </div>
